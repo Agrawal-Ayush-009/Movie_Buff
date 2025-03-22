36882c468a9a1d5e471694f1554ce1522032403c
ayushagrawal <ayushmaserati@gmail.com>
2025-03-23 00:10:07 +0530

    added font

--- app/src/main/java/com/example/moviebuff/ui/theme/Type.kt
+++ app/src/main/java/com/example/moviebuff/ui/theme/Type.kt
@@ -16,7 +16,7 @@ val provider = GoogleFont.Provider(
     certificates = R.array.com_google_android_gms_fonts_certs
 )
 
-val fontName = GoogleFont("Product Sans")
+val fontName = GoogleFont("Lobster Two")
 
 val fontFamily = FontFamily(
 
